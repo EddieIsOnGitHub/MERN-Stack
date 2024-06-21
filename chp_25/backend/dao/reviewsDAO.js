d8571238d3b48b3bb629e25682469e033ed530a7
EddieIsOnGetHub <eduan.vanwaveren@gmail.com>
2024-06-21 10:21:58 +0200

    update files

--- chp_25/backend/dao/reviewsDAO.js
+++ chp_25/backend/dao/reviewsDAO.js
@@ -2,12 +2,12 @@ import mongodb from "mongodb";
 
 const ObjectId = mongodb.ObjectId;
 let reviews;
+
 export default class ReviewsDAO {
   static async injectDB(conn) {
     if (reviews) {
       return;
     }
-    
     try {
       reviews = await conn
         .db(process.env.MOVIEREVIEWS_NS)
