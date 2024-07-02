8babec6c4fd090bad027e63a87a02e72c19fae2a
EddieIsOnGetHub <eduan.vanwaveren@gmail.com>
2024-07-02 10:14:45 +0200

    update files

--- Book-Store-MERN-Stack-main/backend/config.js
+++ Book-Store-MERN-Stack-main/backend/config.js
@@ -2,6 +2,6 @@ export const PORT = 5555;
 
 export const mongoDBURL =
   'mongodb+srv://eddie:ed1234@cluster0.fgtnqyc.mongodb.net/books-collection?retryWrites=true&w=majority';
-  
+   
   // Please create a free database for yourself.
 // This database will be deleted after tutorial
